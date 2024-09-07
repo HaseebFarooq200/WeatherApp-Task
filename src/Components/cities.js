46251528827139eea83acb82d5ef7596a3c6a9be
Muhammad Haseeb Khan <haseebfarooq200@gmail.com>
2024-09-08 02:03:37 +0500

    first-commit

---
+++ src/Components/cities.js
@@ -0,0 +1,212 @@
+export const cities = [
+    {
+        "city": "Karachi",
+        "latitude": 24.8607,
+        "longitude": 67.0011
+    },
+    {
+        "city": "Lahore",
+        "latitude": 31.5497,
+        "longitude": 74.3436
+    },
+    {
+        "city": "Islamabad",
+        "latitude": 33.6844,
+        "longitude": 73.0479
+    },
+    {
+        "city": "Rawalpindi",
+        "latitude": 33.5651,
+        "longitude": 73.0169
+    },
+    {
+        "city": "Faisalabad",
+        "latitude": 31.4181,
+        "longitude": 73.0776
+    },
+    {
+        "city": "Multan",
+        "latitude": 30.1575,
+        "longitude": 71.5249
+    },
+    {
+        "city": "Peshawar",
+        "latitude": 34.0151,
+        "longitude": 71.5249
+    },
+    {
+        "city": "Quetta",
+        "latitude": 30.1798,
+        "longitude": 66.9750
+    },
+    {
+        "city": "Sialkot",
+        "latitude": 32.4927,
+        "longitude": 74.5319
+    },
+    {
+        "city": "Gujranwala",
+        "latitude": 32.1877,
+        "longitude": 74.1945
+    },
+    {
+        "city": "Hyderabad",
+        "latitude": 25.3960,
+        "longitude": 68.3578
+    },
+    {
+        "city": "Sukkur",
+        "latitude": 27.7052,
+        "longitude": 68.8574
+    },
+    {
+        "city": "Bahawalpur",
+        "latitude": 29.3956,
+        "longitude": 71.6820
+    },
+    {
+        "city": "Sargodha",
+        "latitude": 32.0836,
+        "longitude": 72.6710
+    },
+    {
+        "city": "Larkana",
+        "latitude": 27.5572,
+        "longitude": 68.2120
+    },
+    {
+        "city": "Sheikhupura",
+        "latitude": 31.7131,
+        "longitude": 73.9783
+    },
+    {
+        "city": "Dera Ghazi Khan",
+        "latitude": 30.0561,
+        "longitude": 70.6348
+    },
+    {
+        "city": "Chiniot",
+        "latitude": 31.7200,
+        "longitude": 72.9784
+    },
+    {
+        "city": "Jhang",
+        "latitude": 31.2698,
+        "longitude": 72.3169
+    },
+    {
+        "city": "Kasur",
+        "latitude": 31.1163,
+        "longitude": 74.4462
+    },
+    {
+        "city": "Mardan",
+        "latitude": 34.1988,
+        "longitude": 72.0400
+    },
+    {
+        "city": "Mingora",
+        "latitude": 34.7717,
+        "longitude": 72.3608
+    },
+    {
+        "city": "Gujrat",
+        "latitude": 32.5731,
+        "longitude": 74.0783
+    },
+    {
+        "city": "Sahiwal",
+        "latitude": 30.6612,
+        "longitude": 73.1048
+    },
+    {
+        "city": "Wah Cantonment",
+        "latitude": 33.7456,
+        "longitude": 72.7511
+    },
+    {
+        "city": "Mirpur Khas",
+        "latitude": 25.5251,
+        "longitude": 69.0159
+    },
+    {
+        "city": "Nawabshah",
+        "latitude": 26.2483,
+        "longitude": 68.4096
+    },
+    {
+        "city": "Bannu",
+        "latitude": 32.9854,
+        "longitude": 70.6027
+    },
+    {
+        "city": "Kohat",
+        "latitude": 33.5819,
+        "longitude": 71.4493
+    },
+    {
+        "city": "Abbottabad",
+        "latitude": 34.1463,
+        "longitude": 73.2110
+    },
+    {
+        "city": "Jhelum",
+        "latitude": 32.9330,
+        "longitude": 73.7264
+    },
+    {
+        "city": "Sadiqabad",
+        "latitude": 28.3062,
+        "longitude": 70.1307
+    },
+    {
+        "city": "Khanewal",
+        "latitude": 30.3017,
+        "longitude": 71.9321
+    },
+    {
+        "city": "Hafizabad",
+        "latitude": 32.0703,
+        "longitude": 73.6880
+    },
+    {
+        "city": "Kohlu",
+        "latitude": 29.8964,
+        "longitude": 69.2536
+    },
+    {
+        "city": "Loralai",
+        "latitude": 30.3714,
+        "longitude": 68.5974
+    },
+    {
+        "city": "Khuzdar",
+        "latitude": 27.8111,
+        "longitude": 66.6100
+    },
+    {
+        "city": "Dera Murad Jamali",
+        "latitude": 28.5461,
+        "longitude": 68.2230
+    },
+    {
+        "city": "Turbat",
+        "latitude": 25.9911,
+        "longitude": 63.0683
+    },
+    {
+        "city": "Gwadar",
+        "latitude": 25.1266,
+        "longitude": 62.3225
+    },
+    {
+        "city": "Zhob",
+        "latitude": 31.3481,
+        "longitude": 69.4476
+    },
+    {
+        "city": "Umerkot",
+        "latitude": 25.3611,
+        "longitude": 69.7396
+    }
+]
---
+++ src/Components/home.jsx
@@ -0,0 +1,190 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { cities } from './cities';
+import { Input, AutoComplete, Button, Alert } from 'antd';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
+
+const Home = () => {
+    const [error, setError] = useState(null);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const [options, setOptions] = useState([]);
+    const [selectedCity, setSelectedCity] = useState(null);
+    const [displayArray, setDisplayArray] = useState(() => {
+        const savedData = localStorage.getItem('weatherDetails');
+        return savedData ? JSON.parse(savedData) : [];
+    });
+
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+    const fetchWeatherData = async () => {
+        try {
+            const response = await axios.get(`http://api.openweathermap.org/data/3.0/onecall?lat=${selectedCity.latitude}&lon=${selectedCity.longitude}&units=metric&appid=${API_KEY}`);
+            const cityWeather = {
+                ...response.data,
+                city: selectedCity.city
+            };
+            const newWeatherArray = [...displayArray, cityWeather];
+            setDisplayArray(newWeatherArray);
+            localStorage.setItem('weatherDetails', JSON.stringify(newWeatherArray));
+        } catch (err) {
+            setError(err);
+        }
+    };
+
+    const handleSearch = (value) => {
+        if (value) {
+            setInputValue(value);
+            const filteredCities = cities
+                .filter(cityObj => cityObj.city.toLowerCase().includes(value.toLowerCase()))
+                .map(cityObj => ({
+                    value: cityObj.city,
+                    label: cityObj.city,
+                    cityObj
+                }));
+            setOptions(filteredCities);
+            setError('');
+
+        } else {
+            setOptions([]);
+        }
+    };
+
+    const handleSelect = (value, option) => {
+        const selected = option.cityObj;
+        setSelectedCity(selected);
+        setInputValue(value);
+
+    };
+
+    const handleSubmit = () => {
+        setIsSubmitting(true);
+
+        if (!selectedCity) {
+            setError('Please select a valid city.');
+            setIsSubmitting(false);
+
+            return;
+        }
+        setInputValue('');
+        fetchWeatherData();
+        setSelectedCity(null)
+        setIsSubmitting(false);
+
+    };
+
+    const handleDelete = (index) => {
+        const updatedArray = displayArray.filter((_, i) => i !== index);
+        setDisplayArray(updatedArray);
+        localStorage.setItem('weatherDetails', JSON.stringify(updatedArray));
+    };
+
+
+    const CustomLeftArrow = ({ onClick }) => {
+        return (
+            <button
+                className="absolute  left-[-8px] top-1/2 transform -translate-y-1/2 text-black bg-transparent rounded-full z-50"
+                onClick={onClick}
+            >
+                <MdNavigateBefore size={24} />
+            </button>
+        );
+    };
+
+    const CustomRightArrow = ({ onClick }) => {
+        return (
+            <button
+                className="absolute  right-[-8px] top-1/2 transform -translate-y-1/2 text-black bg-transparent rounded-full"
+                onClick={onClick}
+            >
+                <MdNavigateNext size={24} />
+            </button>
+        );
+    };
+
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+    return (
+        <>
+            <div className='flex flex-col mt-4 bg-white max-w-md mx-auto p-4 rounded-lg shadow-lg text-center'>
+                <h2 className='font-bold text-start text-[20px]'>Weather updates</h2>
+                {error && (
+                    <Alert message={error} type="error" showIcon className="mb-4" />
+                )}
+                <AutoComplete
+                    options={options}
+                    onSearch={handleSearch}
+                    onSelect={handleSelect}
+                    placeholder="Search city"
+                    value={inputValue}
+                    onChange={setInputValue}
+                    className='text-start mt-8'
+                >
+                    <Input />
+                </AutoComplete>
+
+                <Button className='w-[20%] mt-4' loading={isSubmitting}
+                    type="primary" onClick={handleSubmit}>Submit</Button>
+            </div>
+
+            <div className='mt-10 '>
+                {displayArray.length !== 0 ? (
+                    <Carousel
+                        className='w-[90%] mx-auto '
+                        responsive={responsive}
+                        infinite={true}
+                        customLeftArrow={<CustomLeftArrow />}
+                        customRightArrow={<CustomRightArrow />}
+                        autoPlay={true}
+                    >
+                        {displayArray.map((weather, index) => {
+                            return (
+                                <div key={index} className="p-4 bg-[#ffce71] rounded-lg  text-start mb-4 mr-2 ml-2">
+                                    <h1 className="text-2xl font-bold mb-4">Weather in {weather.city}</h1>
+                                    <div className="flex items-center justify-start">
+                                        <img
+                                            src={`http://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`}
+                                            alt="Weather Icon"
+                                        />
+                                        <div className="ml-4 text-left">
+                                            <h2 className="text-xl font-semibold mb-2">{weather.current.weather[0].description}</h2>
+                                            <p className="text-lg">Temperature: {weather.current.temp}°C</p>
+                                            <p className="text-lg">Humidity: {weather.current.humidity}%</p>
+                                        </div>
+                                    </div>
+                                    <div className='flex justify-end'>
+                                        <Button className="mt-4" type="primary" danger onClick={() => handleDelete(index)}>Delete</Button>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </Carousel>
+                ) : (
+                    <div className='font-bold  text-[20px]'>No data to show</div>
+                )}
+
+            </div>
+        </>
+    );
+};
+
+export default Home;
